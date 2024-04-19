e745e7ecb74796cfb56a7fd3ca65c531de73477f
Maud6930 <maud.hue6@gmail.com>
2024-04-19 14:59:07 +0200

    changement du champ ID

--- skinapp/src/kinslist.jsx
+++ skinapp/src/kinslist.jsx
@@ -5,12 +5,12 @@ function KinList() {
   const kins = useStore(state => state.kins);
   const addKin = useStore(state => state.addKin);
   const removeKin = useStore(state => state.removeKin);
-  const [nouvKin, setNouvKin] = useState({ id: '', name: '', description: '', tags: [], languages: [], activated: false }); // initialise l'état d'un nouveau kin
+  const [nouvKin, setNouvKin] = useState({ name: '', description: '', tags: [], languages: [], activated: false }); // initialise l'état d'un nouveau kin
 
   const handleAddKin = () => { // fonction handleAddKin conçue pour gérer l'ajout d'un nouveau "kin" à une liste /appelée lorsque l'utilisateur clique sur un bouton pour soumettre le formulaire.
     addKin({ ...nouvKin, id: Date.now().toString() });// La fonction génère un nouvel id pour le nouveau kin en utilisant Date.now().toString(). afin de generer un Id unique au moment de sa création (nb de milliseconde écoulées depuis le 01/01/70 00h)
     // On passe nouKin dans la fonction addKin avec le nouvel ID 
-    setNouvKin({ id: '', name: '', description: '', tags: [], languages: [], activated: false });//Après l'ajout du nouveau kin = état local nouvKin est réinitialisé à ses valeurs par défaut
+    setNouvKin({ name: '', description: '', tags: [], languages: [], activated: false });//Après l'ajout du nouveau kin = état local nouvKin est réinitialisé à ses valeurs par défaut
   };
      
   const handleChange = (e) => {
@@ -32,7 +32,6 @@ function KinList() {
         ))}
       </ul>
       <div>
-        <input type="text" name="id" value={nouvKin.id} onChange={handleChange} placeholder="ID" />
         <input type="text" name="name" value={nouvKin.name} onChange={handleChange} placeholder="Name" />
         <input type="text" name="description" value={nouvKin.description} onChange={handleChange} placeholder="Description" />
         <input type="text" name="tags" value={nouvKin.tags} onChange={handleChange} placeholder="Tags" />
