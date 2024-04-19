9fef8521d18aec9960ab4c4a094e3363b8cc8718
Maud6930 <maud.hue6@gmail.com>
2024-04-19 14:52:04 +0200

    ajout/suppression + tailwindcss
    1. Ajout de boutons et formulaires pour créer ou supprimer un kin
    2. Mise en place de tailwindcss

--- skinapp/src/App.jsx
+++ skinapp/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-
 import './App.css'
 import Kinslist from './kinslist'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <div>
-        <h1>Liste des kins</h1>
+        <h1 className="text-3xl font-bold underline">Liste des kins</h1>
         <Kinslist /> 
       </div>
       /* kinslist = composant permettant l'affichage de la liste*/
--- skinapp/src/kinslist.jsx
+++ skinapp/src/kinslist.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStore from './storekins';
 
 function KinList() {
-  const kins = useStore((state) => state.kins);//extraire la liste kins de useStore / state.kins propriété de l'objet state qui contient la liste liste de données kins
+  const kins = useStore(state => state.kins);
+  const addKin = useStore(state => state.addKin);
+  const removeKin = useStore(state => state.removeKin);
+  const [nouvKin, setNouvKin] = useState({ id: '', name: '', description: '', tags: [], languages: [], activated: false }); // initialise l'état d'un nouveau kin
+
+  const handleAddKin = () => { // fonction handleAddKin conçue pour gérer l'ajout d'un nouveau "kin" à une liste /appelée lorsque l'utilisateur clique sur un bouton pour soumettre le formulaire.
+    addKin({ ...nouvKin, id: Date.now().toString() });// La fonction génère un nouvel id pour le nouveau kin en utilisant Date.now().toString(). afin de generer un Id unique au moment de sa création (nb de milliseconde écoulées depuis le 01/01/70 00h)
+    // On passe nouKin dans la fonction addKin avec le nouvel ID 
+    setNouvKin({ id: '', name: '', description: '', tags: [], languages: [], activated: false });//Après l'ajout du nouveau kin = état local nouvKin est réinitialisé à ses valeurs par défaut
+  };
+     
+  const handleChange = (e) => {
+    const { name, type, checked, value } = e.target; //e.target = ref à l'élément DOM qui déclenche le formulaire
+    setNouvKin(prev => ({
+      ...prev,
+      [name]: type === 'checkbox' ? checked : value
+    }));
+  };
 
   return (
-    <ul>
-      {kins.map((kin) => (
-        <li key={kin.id}> 
-          {kin.name}
-          {kin.description}
-          {kin.tags}
-          {kin.languages}
-          {kin.activated}
-        </li>
-         /* map() = reprend un tableau existant et en reproduit un nouveau sans modifier l'original
-         1.appelle id de chaque kins
-         2. appelle de chaque proporiétés des kins  */
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {kins.map(kin => (
+          <li key={kin.id}>
+            {kin.name} - {kin.description} - {JSON.stringify(kin.tags)} - {JSON.stringify(kin.languages)} - {String(kin.activated)}
+            <button onClick={() => removeKin(kin.id)}>Supprimer</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input type="text" name="id" value={nouvKin.id} onChange={handleChange} placeholder="ID" />
+        <input type="text" name="name" value={nouvKin.name} onChange={handleChange} placeholder="Name" />
+        <input type="text" name="description" value={nouvKin.description} onChange={handleChange} placeholder="Description" />
+        <input type="text" name="tags" value={nouvKin.tags} onChange={handleChange} placeholder="Tags" />
+        <input type="text" name="languages" value={nouvKin.languages} onChange={handleChange} placeholder="Languages" />
+        <input type="checkbox"  id="activatedCheckbox" name="activated"checked={nouvKin.activated} onChange={handleChange} placeholder="Activated"
+        />
+         <label htmlFor="activatedCheckbox">Cliquez sur la checkbox pour activer</label>
+        <button onClick={handleAddKin}>Ajouter Kin</button>
+      </div>
+    </div>
   );
 }
-export default KinList
+
+export default KinList;
