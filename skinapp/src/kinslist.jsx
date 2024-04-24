a439401b9163dde063de8aa1781e46a4808b68a1
Maud6930 <maud.hue6@gmail.com>
2024-04-24 10:33:22 +0200

    changement dans les tyles
    - fond d'écran
    - boutons (les rendre plus accessible et moins agressifs)
    - différenciation : titre, description et informations

--- skinapp/src/kinslist.jsx
+++ skinapp/src/kinslist.jsx
@@ -26,12 +26,12 @@ function KinList() {
        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {kins.map(kin => (
           <div key={kin.id} className="bg-white p-4 rounded-lg shadow-lg transform transition duration-300 hover:scale-105">
-            <h3 className="text-gray-600">{kin.name}</h3>
+            <h3 className="text-2xl font-extrabold text-neutral-400">{kin.name}</h3>
             <p className="text-gray-600">{kin.description}</p>
-            <p className="text-gray-500">Tags: {JSON.stringify(kin.tags)}</p>
-            <p className="text-gray-500">Languages: {JSON.stringify(kin.languages)}</p>
-            <p className="text-sm text-gray-700">Activated: {String(kin.activated)}</p>
-            <button className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 transition duration-300"
+            <p className="text-gray-500 italic">Tags: {JSON.stringify(kin.tags)}</p>
+            <p className="text-gray-500 italic">Languages: {JSON.stringify(kin.languages)}</p>
+            <p className="text-sm text-gray-700 font-semibold italic">Activated: {String(kin.activated)}</p>
+            <button className="mt-4 bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-700 transition duration-300"
               onClick={() => removeKin(kin.id)}>Supprimer</button>
             </div>
         ))}
@@ -45,7 +45,7 @@ function KinList() {
           <input type="checkbox" id="activatedCheckbox" name="activated" checked={nouvKin.activated} onChange={handleChange} className="checkbox checkbox-primary" />
           <label htmlFor="activatedCheckbox" className="ml-2">Cliquez sur la checkbox pour activer</label>
         </div>
-        <button onClick={handleAddKin} className="btn btn-primary mt-4">Ajouter Kin</button>
+        <button onClick={handleAddKin} className="mt-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 transition duration-300">Ajouter Kin</button>
       </div>
     </div>
   );
